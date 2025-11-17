5d478ec714d729b7fda0abf15424b700fd6a2ef1
Md Khorshed Sheikh <sheikhkhorshed1668@gmail.com>
2025-11-17 12:17:56 +0600

    home  controller  added and edited

---
+++ src/main/java/com/example/khorshed_2207013_gpa_calculator_builder/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.khorshed_2207013_gpa_calculator_builder.controller;
+
+import com.example.khorshed_2207013_gpa_calculator_builder.SceneNavigator;
+import javafx.fxml.FXML;
+
+public class HomeController {
+
+    @FXML
+    private void handleStartCalculator() {
+        SceneNavigator.showCalculator();
+    }
+}
